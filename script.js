36bbff52d7a68e0bc5d5c236285ba4b35219427c
anaqvi02 <112963557+anaqvi02@users.noreply.github.com>
2023-12-15 09:17:23 -0500

    Updates
    Still does not work.

---
+++ script.js
@@ -0,0 +1,52 @@
+let userInput = {
+    lastSentence: '',
+    currentInput: ''
+};
+
+let typingTimer;
+let prediction;
+
+const textField = document.getElementById('text_field');
+let log = document.getElementById('log');
+const apiinput = document.getElementById('API input')
+
+
+textField.addEventListener('input', function(e) {
+    clearTimeout(typingTimer);
+
+    const sentences = e.target.value.match(/[^\.!\?]+[\.!\?]+/g);
+    if (sentences) {
+        userInput.lastSentence = sentences[sentences.length - 1];
+    }
+
+    typingTimer = setTimeout(function() {
+        userInput.currentInput = e.target.value;
+
+        const prompt = userInput.currentInput + userInput.lastSentence;
+
+        fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': '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' // replace with your actual OpenAI API key
+            },
+            body: JSON.stringify({
+                'prompt': prompt,
+                'max_tokens': 10
+            })
+            
+        })
+        .then(response => response.json())
+        .then(data => {
+            prediction = data.choices[0].text;
+            console.log(prediction);
+            document.getElementById('ghost_text').innerText = prediction;
+        })
+        .catch(error => console.error('Error:', error));
+
+
+    }, 1000); // 1 second delay
+});
+
+
+
